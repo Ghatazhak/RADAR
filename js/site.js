8e048a7830f57fb6617717e3ae48e1b2d7bb910b
Bart Lyons <lyons.bart@protonmail.com>
2022-11-30 19:02:45 -0500

    Safety Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+// get the string from the page.
+function getString(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString); 
+    displayString(revString);
+}
+
+
+
+// reverse the string.
+function reverseString(userString){
+
+    let revString = "";
+
+
+    // reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+
+
+// display the reverse string.
+function displayString(revString){
+    // write to the DOM
+    document.getElementById("message").innerHTML = `Your string reversed is: ${revString}`;
+
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
