26f33d2c51d8c98a2e3b4fd95490a6808079458c
Bart Lyons <lyons.bart@protonmail.com>
2022-11-30 20:05:16 -0500

    Done with my version.

--- js/site.js
+++ js/site.js
@@ -4,15 +4,46 @@ function getString(){
 
     let userString = document.getElementById("userString").value;
 
-    let revString = reverseString(userString); 
-    displayString(revString);
+    userString = cleanString(userString);
+    
+    let revString = reverseString(userString);
+
+    let isPalindrome = checkForPalindrome(userString,revString);
+
+    if(isPalindrome){
+        displayString(revString);
+    }
+    
 }
 
+function cleanString(userString){
+    
+    // other ways to change strings
+    // cleanString = userString.toLowerCase();
+    // cleanString = cleanString.replace(' ', '');
+    // cleanString = cleanString.split(' ').join('')
+
+    userString = userString.replace(/[^a-zA-Z]/g, "").toLowerCase();
+
+    // returns string
+    return userString;
+}
+
+
+function checkForPalindrome(userString,revString){
+    //check if the string is a palindrome
+    if(userString == revString){
+        return true;
+    } else{
+        return false;
+    }
+}
 
 
 // reverse the string.
 function reverseString(userString){
 
+    
     let revString = "";
 
 
@@ -26,12 +57,14 @@ function reverseString(userString){
 }
 
 
-
 // display the reverse string.
 function displayString(revString){
     // write to the DOM
-    document.getElementById("message").innerHTML = `Your string reversed is: ${revString}`;
+    document.getElementById("message").innerHTML = `Your string is a palindrome: ${revString}`;
 
     // show the alert box
     document.getElementById("alert").classList.remove("invisible");
 }
+
+
+
